03c7eb763f9b31936138b73b5f294f46fa7483da
Sumaiya Rimu <sumaiyarimu22@gmail.com>
2023-05-04 13:02:28 +0600

    courses api done

--- prisma/courses.js
+++ prisma/courses.js
@@ -1,12 +1,12 @@
-//controlar funcation
 import prisma from "./prisma";
+
 //Get all courses
 export const getAllCourses = async () => {
   const courses = await prisma.course.findMany({});
   return courses;
 };
 
-//Get a single courses
+//Get a single course
 export const getCourse = async (id) => {
   const course = await prisma.course.findUnique({
     where: { id },
