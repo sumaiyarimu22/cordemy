074d49545a1d9689f17db9c886addf9f7c29d939
Sumaiya Rimu <sumaiyarimu22@gmail.com>
2023-06-16 18:37:19 +0600

    chnages

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 const image =
-  "https://images.pexels.com/photos/4145153/pexels-photo-4145153.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
+  "https://images.pexels.com/photos/5466286/pexels-photo-5466286.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
 const HeroSection = () => {
   return (
     <div
